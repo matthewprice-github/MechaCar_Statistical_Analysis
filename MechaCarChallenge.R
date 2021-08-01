dc98adccb328769405adc6940151397a3cd49825
matthewprice-github <82009783+matthewprice-github@users.noreply.github.com>
2021-08-01 11:04:22 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,4 +10,14 @@ ScoilTable <- read.csv('Suspension_Coil.csv')
 total_summary <- ScoilTable %>% summarise(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lots_summary <- ScoilTable %>% group_by(Manufacturing_Lot) %>%  summarise(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
-                                        
+
+t.test(ScoilTable$PSI, mu=1500)
+
+lot1 <-  subset(ScoilTable, Manufacturing_Lot=='Lot1')
+lot2 <-  subset(ScoilTable, Manufacturing_Lot=='Lot2')
+lot3 <-  subset(ScoilTable, Manufacturing_Lot=='Lot3')
+
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500)
+
